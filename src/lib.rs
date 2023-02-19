f35e83d60f95c02922a72837afab12d5a62125f4
Pol Milian <pol.milian.dev@gmail.com>
2023-02-19 14:26:27 +0800

    make a request

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::Request;
+use web_sys::RequestInit;
+#[wasm_bindgen]
+pub async fn get_coin_price(coin: String) -> Result<String, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(web_sys::RequestMode::Cors);
 
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
-#[wasm_bindgen]
-pub fn hello_world() {
-    alert("hello world");
+    let url = format!("https://api.coingecko.com/api/v3/coins/{coin}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false");
+    let request = Request::new_with_str_and_init(&url, &opts).unwrap();
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request))
+        .await
+        .unwrap();
+    let resp = resp_value.dyn_into::<web_sys::Response>().unwrap();
+    let text = JsFuture::from(resp.text()?).await?.as_string().unwrap();
+
+    Ok(text)
 }
